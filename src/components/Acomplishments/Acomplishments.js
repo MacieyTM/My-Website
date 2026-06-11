1130950ffb0b5884dd3eb78c47c2a05589da7cbe
MacieyTM <mghej3@wp.pl>
2026-06-11 22:50:02 +0200

    Interface changes

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -6,7 +6,7 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 const data = [
   { number: 2026, text: '4 lata kom. dośw. w firmie RBC Polska'},
   { number: 2024, text: 'Obrona pracy inżynierskiej na 5.0'},
-  { number: 2020, text: 'Złoty Indeks Politechniki Krakowskiej'},
+  { number: 2020, text: 'Złoty Indeks PK'},
   { number: 2020, text: 'Złoty Indeks Politechniki Śląskiej'},
   { number: 2019, text: 'Reprezentant Polski w grze TrackMania'},
   { number: 2018, text: 'Wyróżniony betatester'}
